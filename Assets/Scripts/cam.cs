3191ebf02daccd603d2eed8126602e5347e62aac
Sbstyn <54476814+Sbstyn@users.noreply.github.com>
2020-09-25 17:13:09 +0200

    Add files via upload

---
+++ Assets/Scripts/cam.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+    public GameObject target;
+     void Update()
+     {
+        gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y);
+     }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 0;
+    public bool isgrounded = false;
+    public float rot = 0;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    void Update()
+    {
+        Debug.Log(isgrounded);
+
+        if (Input.GetKey(KeyCode.Mouse0) && isgrounded == false)
+        {
+            rb.MoveRotation(Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + rot));
+            rot = rot + 5;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && isgrounded == true)
+        {
+            if (speed < 10)
+            {
+                speed = speed + 0.1f;
+                transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self);
+            }
+            else
+            {
+                speed = 10;
+                transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && isgrounded == false)
+        {
+            if (speed > 0)
+            speed = speed - 0.1f;
+            transform.Translate(rb.velocity.normalized * Time.deltaTime * speed);
+        }
+    }
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground" && Input.GetKey(KeyCode.Mouse0))
+        {
+            rb.velocity = Vector3.zero;
+        }
+        if (col.gameObject.tag == "ground")
+        {
+            isgrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground" && Input.GetKey(KeyCode.Mouse0))
+        {
+            isgrounded = false;
+        }
+    }
+}
